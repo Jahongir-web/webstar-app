01b0f9d1d289064b994cc7b6b8f5003d100369f3
Jahongir-web <jahongirmh@gmail.com>
2022-06-25 17:26:04 +0500

    change method like and dislike

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -10,8 +10,8 @@ router.put("/post/:id", authRole(), postCtrl.updatePost)
 router.delete("/post/:id", authRole(), postCtrl.deletePost)
 router.get("/my", authRole(), postCtrl.getUserPosts)
 router.get("/post/:id", postCtrl.getById)
-router.post("/like/:id", authRole(), postCtrl.like)
-router.post("/dislike/:id", authRole(), postCtrl.dislike)
+router.get("/like/:id", authRole(), postCtrl.like)
+router.get("/dislike/:id", authRole(), postCtrl.dislike)
 
 
 
